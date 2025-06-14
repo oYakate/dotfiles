9e18d34ac7a7b9e8929bd2f50852298c98234365
oYakate <lukasz.jozef.kroll@gmail.com>
2025-06-14 16:50:04 +0200

    basically created the install script

--- install.sh
+++ install.sh
@@ -1,5 +1,42 @@
+#!/bin/bash
+
+if [ "$(basename "$PWD")" = "dotfiles" ]; then
+    dotfiles_dir=$(pwd)
+else
+    cd $HOME
+    echo "cloning dotfiles...\n"
+
+    git clone https://github.com/oYakate/dotfiles.git
+    cd dotfiles
+    dotfiles_dir=$(pwd)
+fi
+
 sudo pacman -Sy yay --noconfirm
 
 yay -Sy adobe-source-han-sans-jp-fonts bibata-cursor-theme-bin blueman bluez-utils catppuccin-gtk-theme-mocha cava code eza fastfetch fzf git glow htop hyprland iwd jq kitty lxappearance nano nautilus neovim networkmanager network-manager-applet opentabletdriver otf-takao pacman-contrib pavucontrol rofi-wayland spotify swaybg thunar tty-clock vesktop vim w3m waybar wget wl-clipboard xournalpp zsh --noconfirm
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Creating a backup of your .config directory..."
+mv $HOME/.config $HOME/.config-bck
+
+echo "installing dotfiles...\n"
+
+
+echo "do you want to install oh my zsh? [y/n] "
+read answer
+
+case "$answer" in
+  [yY])
+    echo "Installing zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+    ;;
+  [nN])
+    echo "Skipping..."
+    ;;
+  *)
+    echo "Invalid input. Skipping.."
+    ;;
+esac
+
+
+
